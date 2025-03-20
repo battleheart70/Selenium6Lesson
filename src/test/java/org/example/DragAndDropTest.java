645d6e6af03f40049faa57a250ce9fd230337dab
battleheart70 <battleheart70@gmail.com>
2025-03-20 18:50:46 +0100

    Added first tests

--- src/test/java/org/example/DragAndDropTest.java
+++ src/test/java/org/example/DragAndDropTest.java
@@ -43,16 +43,27 @@ public class DragAndDropTest {
         Point finalPosition = draggable.getLocation();
         Assertions.assertNotEquals(initialPosition, finalPosition, "Элемент не переместился!");
 
-        // Проверяем, что draggable и target теперь пересекаются (по координатам X и Y)
-        int draggableX = draggable.getLocation().getX();
-        int draggableY = draggable.getLocation().getY();
-        int targetX = target.getLocation().getX();
-        int targetY = target.getLocation().getY();
-
-        Assertions.assertTrue(draggableX >= targetX && draggableY >= targetY,
-                "Перетаскиваемый элемент не оказался в рамке");
+        Assertions.assertTrue(isIntersecting(draggable, target), "Элементы не пересекаются");
     }
 
+    private boolean isIntersecting(WebElement element1, WebElement element2) {
+        Rectangle rect1 = element1.getRect();
+        Rectangle rect2 = element2.getRect();
 
+        int left1 = rect1.getX();
+        int top1 = rect1.getY();
+        int right1 = left1 + rect1.getWidth();
+        int bottom1 = top1 + rect1.getHeight();
+
+        int left2 = rect2.getX();
+        int top2 = rect2.getY();
+        int right2 = left2 + rect2.getWidth();
+        int bottom2 = top2 + rect2.getHeight();
+
+        return left1 < right2 &&
+                right1 > left2 &&
+                top1 < bottom2 &&
+                bottom1 > top2;
+    }
 
 }
